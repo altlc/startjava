af795ec36d9aa369abc6f255ca1702261e9b322f
altlc <altrealm@gmail.com>
2020-10-26 19:26:37 +0300

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,25 @@
+public class Variable {	
+    public static void main(String[] args) {
+        byte CPUCores = 8;
+        short RAMMegabytes = 8192;
+        int SaundSampleRateHZ = 48000;
+        long HDDSizeBytes = 696411309;
+        float CPUFreqGHZ = 3.2F;
+        double OSDriveSizeGB = 9.121932452;
+        char OSDrive = 'C';
+        boolean IsX86 = true;
+
+		System.out.println("CPUCores = " + CPUCores);
+	    System.out.println("RAMMegabytes = " + RAMMegabytes);
+	    System.out.println("SaundSampleRateHZ = " + SaundSampleRateHZ);
+	    System.out.println("HDDSizeBytes = " + HDDSizeBytes);
+	    System.out.println("CPUFreqGHZ = " + CPUFreqGHZ);
+	    System.out.println("OSDriveSizeGB = " + OSDriveSizeGB);
+	    System.out.println("OSDrive = " + OSDrive);
+	    System.out.println("IsX86 = " + IsX86);
+    }
+
+
+
+
+}
