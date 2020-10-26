c01bb4e111c5c5fc98a667c2980b288fee88acc6
altlc <altrealm@gmail.com>
2020-10-26 20:41:26 +0300

    Variables names

--- Variable.java
+++ Variable.java
@@ -1,20 +1,20 @@
 public class Variable {
 	public static void main(String[] args) {
 		byte cpuCores = 8;
-		short ramMB = 8192;
-		int  soundSampleRateHZ = 48000;
+		short ramMb = 8192;
+		int  soundSampleRateHz = 48000;
 		long hddSizeB = 696411309;
-		float cpuFreqGHZ = 3.2F;
-		double osDriveSizeGB = 9.121932452;
+		float cpuFreqGhz = 3.2F;
+		double osDriveSizeGb = 9.121932452;
 		char osDrive = 'C';
 		boolean isX86 = true;
 
 		System.out.println("cpuCores = " + cpuCores);
-		System.out.println("ramMB = " + ramMB);
-		System.out.println("soundSampleRateHZ = " + soundSampleRateHZ);
+		System.out.println("ramMb = " + ramMb);
+		System.out.println("soundSampleRateHz = " + soundSampleRateHZ);
 		System.out.println("hddSizeB = " + hddSizeB);
-		System.out.println("cpuFreqGHZ = " + cpuFreqGHZ);
-		System.out.println("osDriveSizeGB = " + osDriveSizeGB);
+		System.out.println("cpuFreqGhz = " + cpuFreqGhz);
+		System.out.println("osDriveSizeGb = " + osDriveSizeGb);
 		System.out.println("osDrive = " + osDrive);
 		System.out.println("isX86 = " + isX86);
 	}
