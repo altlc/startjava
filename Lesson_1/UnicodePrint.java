a35e6ce00b4d8590f7a1809350ca77593f56f414
altlc <altrealm@gmail.com>
2020-11-09 10:59:38 +0300

    GuessTheNumber fixes

--- Lesson_1/UnicodePrint.java
+++ Lesson_1/UnicodePrint.java
@@ -1,9 +1,9 @@
 public class UnicodePrint {
 	public static void main(String[] args) {
-		char UnicodeChar = 33;
-		while (UnicodeChar <= 126) {
-			System.out.println(UnicodeChar);
-			UnicodeChar++;
+		char unicodeChar = 33;
+		while (unicodeChar <= 126) {
+			System.out.println(unicodeChar);
+			unicodeChar++;
 		}
 	}
 }
