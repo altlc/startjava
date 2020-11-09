06c60e858c9b83ffd26ffb745d909b4edcc45218
altlc <altrealm@gmail.com>
2020-11-09 10:57:51 +0300

    UnicodePrint.java

--- Lesson_1/UnicodePrint.java
+++ Lesson_1/UnicodePrint.java
@@ -1,5 +1,9 @@
 public class UnicodePrint {
 	public static void main(String[] args) {
-
+		char UnicodeChar = 33;
+		while (UnicodeChar <= 126) {
+			System.out.println(UnicodeChar);
+			UnicodeChar++;
+		}
 	}
 }
