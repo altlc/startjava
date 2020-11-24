717e1dd1a932332b4a88be867b7c9389131b5094
altlc <altrealm@gmail.com>
2020-11-24 10:28:22 +0300

    Wolf, incapsulation

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,49 +1,21 @@
 public class WolfTest {
 	public static void main(String[] args) {
 		Wolf myWolf = new Wolf();
-		myWolf.age = 5;
-		myWolf.name = "Werewolf";
-		myWolf.weight = 20.0f;
-		myWolf.color = "grey";
-		myWolf.gender = "male";
+		myWolf.setAge((byte)5);
+		myWolf.setName("Werewolf");
+		myWolf.setWeight(20.0f);
+		myWolf.setColor("grey");
+		myWolf.setGender("male");
 
-		System.out.println("Wolf name: " + myWolf.name);
-		System.out.println("Wolf age: " + myWolf.age);
-		System.out.println("Wolf gender: " + myWolf.gender);
-		System.out.println("Wolf color: " + myWolf.color);
-		System.out.println("Wolf weight: " + myWolf.weight);
+		System.out.println("Wolf name: " + myWolf.getName());
+		System.out.println("Wolf age: " + myWolf.getAge());
+		System.out.println("Wolf gender: " + myWolf.getGender());
+		System.out.println("Wolf color: " + myWolf.getColor());
+		System.out.println("Wolf weight: " + myWolf.getWeight());
 
 		myWolf.go();
 		myWolf.sit();
 		myWolf.hunt();
 		myWolf.howl();
-	}
-}
-
-public class Wolf {
-	public String gender;
-	public String name;
-	public float weight;
-	public byte age;
-	public String color;
-
-	public void go() {
-		System.out.println("Go");
-	}
-
-	public void run() {
-		System.out.println("Run");
-	}
-
-	public void sit() {
-		System.out.println("Sit");
-	}
-
-	public void hunt() {
-		System.out.println("Hunt");
-	}
-
-	public void howl() {
-		System.out.println("howl");
 	}
 }
